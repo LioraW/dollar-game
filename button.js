02dd512cd6433766b666cbdbf2cfc45dfe78ad5a
lcwdmg <lcwdmg@umsystem.edu>
2021-10-21 17:47:25 -0400

    add button class and create undo button (functionality does not work yet)

---
+++ button.js
@@ -0,0 +1,38 @@
+class Button
+{
+    constructor(text,x, y, width, height, onClick) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.onClick = onClick;
+    }
+    // checks is the mouse has been pressed over the button
+    mouse_listener()
+    {
+        if ( mouse_downed &&
+            mouseX > this.x && mouseX < this.x+this.width &&
+            mouseY > this.y && mouseY < this.y + this.height)
+        {
+            // reset the mouse_downed and mouse_upped functions
+            mouseReset();
+            // then call the passed in function
+            this.onClick();
+
+        }
+    }
+    draw()
+    {
+        this.mouse_listener();
+        fill(255,255,255);
+        rect(this.x, this.y, this.width, this.height, 7); //outline
+        fill(158);
+        rect(this.x, this.y, this.width, this.height, 7); //shade
+        textAlign(CENTER,CENTER);
+        fill(0,0,0);
+        text(this.text, this.x+(this.width/2), this.y+(this.height/2));
+
+    }
+}
+
--- game.js
+++ game.js
@@ -7,10 +7,30 @@ class Game
         this.lastMove = this.currentGraph.lastMove; //will be id of the node that was last clicked, for now, make it invalid
         this.startingGraph = this.currentGraph;
         this.gameWon = false;
+        this.undo = new Button(
+            "undo",
+            100,
+            300,
+            60,
+            30,
+            () => {//anonymous function to call all the things it needs to do
+                print("clicked");
+                this.take();
+                this.currentGraph.reset_last_move();
+            }
+        );
     }
+    take()
+    {
+        if (this.currentGraph.lastMove !== -1){
+            this.currentGraph.nodes[this.currentGraph.lastMove].give(-1);
+        }
+    }
+
     draw()
     {
         this.currentGraph.draw();
+        this.undo.draw();
     }
 
 }
