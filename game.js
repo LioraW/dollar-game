df1776a6d16b29e6768a2eeeb2705e35f37f5495
lcwdmg <lcwdmg@umsystem.edu>
2021-10-21 21:54:18 -0400

    functionality works for undo button!

--- game.js
+++ game.js
@@ -4,33 +4,22 @@ class Game
         this.currentGraph = new Graph(node_size, edges_max, provable, money_range)
         this.currentGraph.populate_nodes();
         this.currentGraph.populate_edges();
-        this.lastMove = this.currentGraph.lastMove; //will be id of the node that was last clicked, for now, make it invalid
-        this.startingGraph = this.currentGraph;
+        this.startingGraph = this.currentGraph; //needs a copy constructor, this won't work
         this.gameWon = false;
-        this.undo = new Button(
-            "undo",
-            100,
-            300,
-            60,
-            30,
-            () => {//anonymous function to call all the things it needs to do
-                print("clicked");
-                this.take();
-                this.currentGraph.reset_last_move();
-            }
-        );
-    }
-    take()
-    {
-        if (this.currentGraph.lastMove !== -1){
-            this.currentGraph.nodes[this.currentGraph.lastMove].give(-1);
-        }
+        this.undo = new Button("undo", 100, 300, 60, 30, () => {
+                console.log("clicked!")
+                if (this.currentGraph.lastMove !== -1){
+                    this.currentGraph.nodes[this.currentGraph.lastMove].give(-1);
+                }
+                this.currentGraph.set_last_move(-1);}
+         );
     }
 
     draw()
     {
+        this.undo.draw(); //apparently, buttons need to happen before the graph for the graph to be responsive
         this.currentGraph.draw();
-        this.undo.draw();
+
     }
 
 }
